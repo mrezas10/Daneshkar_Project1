8bf325fe8152a4a124dc034cba3c8c42af0e67bf
zahra nuori sorkhani <zahranuori.1375@gmail.com>
2021-10-09 18:50:42 +0330

    responsive desktop

---
+++ JS/dropdown.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("menu");
+    if (x.className === "topnav") {
+        x.className += " opened";
+    } else {
+        x.className = "topnav";
+    }
+}
