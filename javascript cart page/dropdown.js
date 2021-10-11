82e9700504f516a86cffe7fbd8f98280892ffbb7
narjes <narjessheikhali@gmail.com>
2021-10-11 14:47:05 +0330

    clean code cart

---
+++ javascript cart page/dropdown.js
@@ -0,0 +1,9 @@
+
+    function myFunction() {
+    var x = document.getElementById("menu");
+    if (x.className === "topnav") {
+    x.className += " opened";
+} else {
+    x.className = "topnav";
+}
+}
